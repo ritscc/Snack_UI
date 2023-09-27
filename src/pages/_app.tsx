34c44d6df09f1a1d48b0ef4f38245f2239b56e17
rintaro1029 <tomitaka1029@gmail.com>
2023-09-27 16:31:54 +0900

    ヘッダーコンポーネントの作成

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import Header from "@/components/Header";
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="flex h-full w-full flex-col ">
+      <div className="sticky top-0 z-10">
+        <Header />
+      </div>
+      <div className=" hide-scrollbar h-full overflow-y-auto ">
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
