f7a60230f7a806657b0a9f36b9759eeed198d895
hane <winwinmaomao@gmail.com>
2023-09-27 19:20:39 +0900

    検索欄を追加

---
+++ src/pages/chatHomeCenter.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function chatHomeCenter() {
+    return (
+        <div>        <div className="p-2">chatHomeCenter</div>
+            <div className="border-4"><input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="username" type="text" placeholder="Username"></input></div>
+        </div>
+
+    )
+}
