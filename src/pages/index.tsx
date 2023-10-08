e0bb4063b60dfcfd9409bee9db8e47872470cb9c
hane <winwinmaomao@gmail.com>
2023-10-08 22:46:24 +0900

    余分なimport文を削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,6 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import Chat from "@/components/Chat";
 import Online from "@/components/Online";
-import ChatHomeCenter from "@/components/SearchBar";
 
 const inter = Inter({ subsets: ["latin"] });
 
